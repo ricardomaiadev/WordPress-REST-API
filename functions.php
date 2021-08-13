53492e8c9f357aadfd90cd2516bf40006f576e36
Ricardo <riicardolivera@gmail.com>
2021-08-13 13:13:43 -0400

    refactor(functions): Adicionado rotas de transacao e produto delete, alem de o contador de produtos, e limpeza da tela de recuperacao de senha.

--- functions.php
+++ functions.php
@@ -8,8 +8,13 @@ require_once($template_diretorio . "/custom-post-type/transacao.php");
 require_once($template_diretorio . "/endpoints/usuario_post.php");
 require_once($template_diretorio . "/endpoints/usuario_get.php");
 require_once($template_diretorio . "/endpoints/usuario_put.php");
+
 require_once($template_diretorio . "/endpoints/produto_post.php");
 require_once($template_diretorio . "/endpoints/produto_get.php");
+require_once($template_diretorio . "/endpoints/produto_delete.php");
+
+require_once($template_diretorio . "/endpoints/transacao_post.php");
+require_once($template_diretorio . "/endpoints/transacao_get.php");
 
 
 function get_produto_id_by_slug($slug) {
@@ -26,10 +31,27 @@ function get_produto_id_by_slug($slug) {
      return array_shift($posts);
 }
 
+add_action('rest_pre_serve_request', function() {
+    header('Access-Control-Expose-Headers: X-Total-Count');
+});
+
 function expire_token() {
     return time() + (60 * 60 * 24);
 }
 
 add_action('jwt_auth_expire', 'expire_token');
+
+function my_login_screen() { ?>
+<style type="text/css">
+#login h1 a {
+    background-image: none;
+}
+#backtoblog {
+    display: none;
+}
+    
+<?php }
+
+add_action('login_enqueue_scripts', 'my_login_screen');
 
 ?>
